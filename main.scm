547208055ebf6f4b170c215eddbdb60224692b38
James Jackson <j.e.m.jackson@gmail.com>
2025-06-02 20:09:07 -0500

    added main.scm to run all days.

---
+++ main.scm
@@ -0,0 +1,11 @@
+(use-modules
+ ((octorine aog-2024 day01))
+ ((octorine aog-2024 day02))
+ ((octorine aog-2024 day03))
+ ((octorine aog-2024 day04))
+ ((octorine aog-2024 day05))
+ ((octorine aog-2024 day06))
+ ((octorine aog-2024 day07))
+ ((octorine aog-2024 day08)))
+
+   
